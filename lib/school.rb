7132055166494d8753f3e3e2dee445301261513b
Taevon Jordan <nude.plus@Taevons-MBP.home>
2020-03-31 23:14:20 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def name
+    @name
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade].push name
+    else
+      @roster[name] = [name]
+    end
+  end
+
+  def add_student(name, grade)
+    @add_student
+  end
+end
